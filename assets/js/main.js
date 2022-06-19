92fa8c50d9447ba9a23ab39f0df95e15d48863d6
mish-hash <poluektov23@gmail.com>
2022-06-19 19:32:57 +0300

    2. Work with array

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,43 @@ array.forEach((el)=>{
         console.log(typeof el);
     }
 });
+
+
+/*
+2. Напишите программу, которая спрашивает у пользователя целое число. Создайте массив из случайных элементов, длинной в это число. Случайные чила долны быть в диапазоне 0 до 10. Используйте методы Math.random() и Math.floor() для реализации.
+Выведите содержимое массива консоль.
+Скопируйте массив в новый массив. Каждый третий элемент нового массива умножте на 3.
+Выведите содержимое нового массива в консоли. Подсказака. ПОМНИТЕ ПРО ССЫЛКИ НА МАССИВ ПРИ КОПИРОВАНИИ;)
+*/
+
+let numberInput = parseInt( prompt('Введите целое число'));
+
+while (!Number.isInteger(numberInput)) {
+    numberInput = parseInt( prompt('Ошибка! Введите целое число'));
+};
+
+const arrayRandom = [];
+
+for (let i = 0; i < numberInput; i++) {
+    const randomNumber = Math.floor(Math.random() * (10 - 0 + 1));
+    arrayRandom.push(randomNumber);
+}
+
+const secondArray = arrayRandom.slice();
+
+for (let i = 1; i <= secondArray.lenght; i++) {
+    if (i % 3 === 0){
+        secondArray[i] += 3;
+        continue;
+    }
+}
+
+console.log(secondArray);
+
+
+
+
+
+
+
+
